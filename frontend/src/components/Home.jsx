fdf2fdb9eceb827efa186f4f682fb77544da2e2c
Sebastian <garatti.sebastian@gmail.com>
2020-06-05 15:30:08 +0200

    home changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,70 +1,80 @@
 import React from 'react';
 
-
 const Home = () => {
-     return <div className="container">
-                <div className="row">
-                    <div className="col">
-                        1 of 3
-                    </div>
-             <div className="col-6">
-                 <div>
-                 <div className="card">
-                     <div className="row">
-                         <div className="col-2">
-                             <img src="http://www.pngmart.com/files/4/Circle-PNG-File.png" style={{ width:'100%' }}/>
-                         </div>
-                         <div className="col-10">
-                             <h3>Sebastian Garatti</h3>
-                         </div>
-                     </div>
-                         <img src="..." class="card-img-top" alt="..."/>
-                     <div className="card-body">
-                         <h5 className="card-title">Card title</h5>
-                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                 <a href="#" className="btn btn-primary">Go somewhere</a>
-                     </div>
-                 </div>
-                 </div>
-             </div>
-             
-             <div className="col">
-                 3 of 3
-             </div>
-             
-         </div>
-         <div className="row">
-                    <div className="col">
-                        1 of 3
-                    </div>
-             <div className="col-6">
-                 <div>
-                 <div className="card">
-                     <div className="row">
-                         <div className="col-2">
-                             <img src="http://www.pngmart.com/files/4/Circle-PNG-File.png" style={{ width:'100%' }}/>
-                         </div>
-                         <div className="col-10">
-                             <h3>Uguito Senega</h3>
-                         </div>
-                     </div>
-                         <img src="..." class="card-img-top" alt="..."/>
-                     <div className="card-body">
-                         <h5 className="card-title">Card title</h5>
-                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                 <a href="#" className="btn btn-primary">Go somewhere</a>
-                     </div>
-                 </div>
-                 </div>
-             </div>
-             
-             <div className="col">
-                 3 of 3
-             </div>
-             
-         </div>
-     </div>
-     
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-3">
+          <div className="card text-center">
+            <img
+              src="http://www.pngmart.com/files/4/Circle-PNG-File.png"
+              style={{
+                width: '70px',
+                marginRight: 'auto',
+                marginLeft: 'auto',
+                marginTop: '10px',
+              }}
+            />
+            <h5 className="mt-3">Welcome, Sebastian!</h5>
+            <a href="#">sebbe@gmail.com</a>
+            <a className="mb-3" href="#">Settings</a>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <div>
+            <div className="card">
+              <div className="row">
+                <div className="col-2 p-2">
+                  <img
+                    src="http://www.pngmart.com/files/4/Circle-PNG-File.png"
+                    style={{width: '100%'}}
+                  />
+                </div>
+                <div className="col-10 mt-3">
+                  <h3>Donald Trump</h3>
+                </div>
+              </div>
+              <img src="https://cdn.vox-cdn.com/thumbor/TzfMRZ6j7C8YvxAwFnQIa9Xszwg=/0x0:6000x4000/1200x800/filters:focal(2520x1520:3480x2480)/cdn.vox-cdn.com/uploads/chorus_image/image/66865091/1227623212.jpg.0.jpg" class="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">
+                Sleepy Joe Biden’s 1994 Crime Bill was a total disaster. It was mass incarceration for Black people, many of them innocent. I did Criminal Justice Reform, something Obama & Biden didn’t even try to do - & couldn’t do even if they did try. Biden can never escape his Crime Bill!
+                </p>
+                <a href="#" className="btn btn-primary">Like</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-3">
+          <div className="card p-3">
+            <h6 className="mt-2 mb-2">Search your friends</h6>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Share something"
+            />
+            <a href="#" className="btn btn-primary mt-2">Search</a>
+          </div>
+          <div className="card mt-4 p-3">
+            <h6 className="mt-2 mb-2">Start a post...</h6>
+            <textarea
+              rows="3"
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Share something"
+            />
+            <a href="#" className="btn btn-primary mt-2">Post</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Home;
