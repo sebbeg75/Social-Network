250b7ef787026241ee3ec3a8fac4d35104ffe1c7
Sebastian <garatti.sebastian@gmail.com>
2020-06-05 12:21:11 +0200

    styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Header from './components/Header';
 import Home from './components/Home';
 import Signup from './components/Signup';
