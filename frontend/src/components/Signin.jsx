d387c2b482abbb6f78e4553624a494b2ce0a419e
Sebastian <garatti.sebastian@gmail.com>
2020-06-05 19:28:14 +0200

    images, helpers

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,7 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { showErrorMsg } from '../helpers/message';
+import { showLoading } from '../helpers/loading';
+import isEmpty from 'validator/lib/isEmpty';
+import isEmail from 'validator/lib/isEmail';
+import { signin } from '../api/auth';
 
 const Signin = () => {
-    return <p>Inside Signin component</p>
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        errorMsg:false,
+        loading: false,
+        redirectToDashboard: false,
+    });
+
+    const {
+        email, 
+        password, 
+        errorMsg, 
+        loading,
+        redirectToDashboard,
+        } = formData;
+
+        /* Event Handlers */
+
+        const handleChange = (evt)=> {
+            setFormData({
+                ...formData,
+                [evt.target.name]: evt.target.value,
+                errorMsg: '',
+            });
+         };
+
+         const handleSubmit = (evt) => {
+            evt.preventDefault();
+
+            //client side validation
+            if ( 
+            isEmpty(email) || 
+            isEmpty(password)
+            ){
+                setFormData({
+                    ...formData, 
+                    errorMsg: 'All fields are required',
+                });
+            } else if (!isEmail(email)) {
+                setFormData({
+                    ...formData,
+                    errorMsg: 'Invalid email',
+                });
+            } else {
+                const { email, password } = formData;
+                const data = { email, password };
+
+                setFormData({ ...formData, loading: true});
+
+                signin(data)         
+        }
+    };
+            
+    /* Views */
+
+    const showSigninForm = () => (
+        <form className='signup-form' onSubmit={handleSubmit} noValidate>
+            {/*email*/}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-envelope'></i>
+                    </span>
+                </div>
+                <input
+                    name='email'
+                    value={email}
+                    className='form-control'
+                    placeholder='Email address'
+                    type='email'
+                    onChange={handleChange}
+                />
+            </div>
+            {/*password*/}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-lock'></i>
+                    </span>
+                </div>
+                <input
+                    name='password'
+                    value={password}
+                    className='form-control'
+                    placeholder='Create password'
+                    type='password'
+                    onChange={handleChange}
+                />
+            </div>    
+            {/*login button*/}
+            <div className='form-group'>
+                <button type='submit' className='btn btn-primary btn-block'>
+                    Signin
+                </button>
+            </div>
+            {/*already have account*/}
+            <p className='text-center text-white'>
+                 Dont`t have an account? <Link to='/signup'>Register here</Link>
+            </p>
+        </form>
+    );
+
+    /* Renderer */
+
+    return ( 
+        <div className='signin-container'>
+            <div className='row px-3 vh-100'>
+                <div className='col-md-5 mx-auto align-self-center'>
+                    {errorMsg && showErrorMsg(errorMsg)}
+                    {loading && (
+                        <div className='text-center pb-4'>{showLoading()}</div>)}
+                    {showSigninForm()}
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Signin;
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,8 +1,189 @@
-import React from 'react';
+import React, { useState } from 'react';
+import isEmpty from 'validator/lib/isEmpty';
+import isEmail from 'validator/lib/isEmail';
+import equals from 'validator/lib/equals';
+import { showErrorMsg, showSuccessMsg } from '../helpers/message';
+import { showLoading } from '../helpers/loading';
+import { Link } from 'react-router-dom';
+import { signup } from '../api/auth';
 
 const Signup = () => {
-    return <p>Inside Signup component</p>
-};
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        password2: '',
+        successMsg: false,
+        errorMsg:false,
+        loading: false,
+    });
+    const {
+        username, 
+        email, 
+        password, 
+        password2, 
+        successMsg, 
+        errorMsg, 
+        loading,
+        } = formData;
 
+        /* Event Handlers */
+
+        const handleChange = (evt)=> {
+           setFormData({
+               ...formData,
+               [evt.target.name]: evt.target.value,
+               successMsg: '',
+               errorMsg: '',
+           });
+        };
+
+        const handleSubmit = (evt) => {
+            evt.preventDefault();
+
+            //client side validation
+
+            if (isEmpty(username) || 
+            isEmpty(email) || 
+            isEmpty(password) || 
+            isEmpty(password2)
+            ){
+                setFormData({
+                    ...formData, 
+                    errorMsg: 'All fields are required',
+                });
+            } else if (!isEmail(email)) {
+                setFormData({
+                    ...formData,
+                    errorMsg: 'Invalid email',
+                });
+            } else if (!equals(password, password2)) {
+                setFormData({
+                    ...formData,
+                    errorMsg: 'Passwords do not match',
+                });
+            } else {
+                const {username, email, password} = formData;
+                const data = {username, email, password}
+
+                setFormData({ ...formData, loading: true});
+
+                signup(data)
+                    .then((response) => {
+                        console.log('Axios signup sucess: ', response);
+                        setFormData({
+                            username: '',
+                            email: '',
+                            password: '',
+                            password2: '',
+                            loading: false,
+                            successMsg: response.data.successMessage
+                        })
+                    })
+                    .catch((err) => {
+                        console.log('Axios signup error:', err)
+                        setFormData({ ...formData, loading: false , errorMsg: err.response.data.errorMessage });
+                    });
+            }
+        };
+
+    /* Views */
+
+    const showSignupForm = () => (
+        <form className='signup-form' onSubmit={handleSubmit} noValidate>
+            {/*username*/}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-user'></i>
+                    </span>
+                </div>
+                <input
+                    name='username'
+                    value={username}
+                    className='form-control'
+                    placeholder='Username'
+                    type='text'
+                    onChange={handleChange}
+                />
+            </div>
+            {/*email*/}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-envelope'></i>
+                    </span>
+                </div>
+                <input
+                    name='email'
+                    value={email}
+                    className='form-control'
+                    placeholder='Email address'
+                    type='email'
+                    onChange={handleChange}
+                />
+            </div>
+            {/*password*/}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-lock'></i>
+                    </span>
+                </div>
+                <input
+                    name='password'
+                    value={password}
+                    className='form-control'
+                    placeholder='Create password'
+                    type='password'
+                    onChange={handleChange}
+                />
+            </div>
+            {/*password2*/}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-lock'></i>
+                    </span>
+                </div>
+                <input
+                    name='password2'
+                    value={password2}
+                    className='form-control'
+                    placeholder='Confirm password'
+                    type='password'
+                    onChange={handleChange}
+                />
+            </div>
+            {/*register button*/}
+            <div className='form-group'>
+                <button type='submit' className='btn btn-primary btn-block'>
+                    Sign Up
+                </button>
+            </div>
+            {/*already have account*/}
+            <p className='text-center text-white'>
+                Have an account? <Link to='/signin'>Sign In</Link>
+            </p>
+        </form>
+    );
+    
+    /* Renderer */
+
+    return ( 
+        <div className='signup-container'>
+            <div className='row px-3 vh-100'>
+                <div className='col-md-5 mx-auto align-self-center'>
+                    {successMsg && showSuccessMsg(successMsg)}
+                    {errorMsg && showErrorMsg(errorMsg)}
+                    {loading && (
+                        <div className='text-center pb-4'>
+                        {showLoading()}</div>)}
+                        {showSignupForm()}
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default Signup;
